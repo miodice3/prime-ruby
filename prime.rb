731f2a8b7285d7981587205a8151f549ab6d5f55
Michael Iodice <miodice3@gmail.com>
2020-10-05 11:05:53 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+
+def prime?(n)
+    i=3
+    if n <= 1
+        return false
+    end
+    until i > (n-1)
+        state = n % (i - 1) == 0
+        if state == true
+            return false
+        end
+        i += 1
+    end
+ true
+end
